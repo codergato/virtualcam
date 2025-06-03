036274d1619eaf7f4d6929cb951c5748b1ece697
codergato <codergato@deployyyyer.io>
2025-06-03 21:47:44 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-sudo modprobe -r v4l2loopback
-sudo modprobe v4l2loopback devices=1 video_nr=10              card_label="NoBlueCam" exclusive_caps=1 fmt=bgr24
-python3 virtualcam.py $1
+python3 virtualcam.py 
